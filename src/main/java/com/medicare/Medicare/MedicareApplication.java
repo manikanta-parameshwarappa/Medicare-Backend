8edd786012af8eb2797f6a4d88421da20da9b8a6
Manikanta Parameswarappa <mparameswarappa@plansource.com>
2025-12-04 23:43:24 +0530

    setup springboot project

---
+++ src/main/java/com/medicare/Medicare/MedicareApplication.java
@@ -0,0 +1,14 @@
+package com.medicare.Medicare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class MedicareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicareApplication.class, args);
+	}
+
+}
